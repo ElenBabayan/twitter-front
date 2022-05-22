c2ebf1504a8130d4698fb4152c1756c036e34039
ebabayan <ebabayan@adobe.com>
2022-05-22 14:25:49 +0400

    fix: prettify

--- src/components/profile/follower.js
+++ src/components/profile/follower.js
@@ -1,60 +1,59 @@
-import React, { useEffect, useState } from "react";
-import { Link, useParams } from "react-router-dom";
-import { useSelector } from "react-redux";
+import React, {useEffect, useState} from "react";
+import {Link, useParams} from "react-router-dom";
+import {useSelector} from "react-redux";
 import axios from "axios";
-import { FollowFlex } from "../styles/profile";
-import theme from "../../redux/reducers/theme";
+import {FollowFlex} from "../styles/profile";
 
 const URL = process.env.REACT_APP_SERVER_URL;
 
 const Follower = (props) => {
-  const [response, setResponse] = useState(null);
+    const [response, setResponse] = useState(null);
 
-  const { user } = props;
+    const {user} = props;
 
-  const { username } = useParams();
-  const myId = useSelector((state) => state.profile.user.id);
-  const theme = useSelector((state) => state.theme);
+    const {username} = useParams();
+    const myId = useSelector((state) => state.profile.user.id);
+    const theme = useSelector((state) => state.theme);
 
-  useEffect(() => {
-    // ComponentDidMount
-    (async () => {
-      const res = await axios.get(
-        `${URL}/follow/details?id=${user.id}&myId=${myId}`
-      );
-      setResponse({
-        followers: res.data.followers,
-        following: res.data.following,
-      });
-    })();
-  }, [user]);
+    useEffect(() => {
+        // ComponentDidMount
+        (async () => {
+            const res = await axios.get(
+                `${URL}/follow/details?id=${user.id}&myId=${myId}`
+            );
+            setResponse({
+                followers: res.data.followers,
+                following: res.data.following,
+            });
+        })();
+    }, [user]);
 
-  if (!response) return <React.Fragment></React.Fragment>;
+    if (!response) return <React.Fragment></React.Fragment>;
 
-  return (
-    <FollowFlex>
-      <div>
-        <Link to={`/profile/${username}/following`}>
-          <p>
-            <span style={{ color: theme.color }}>
+    return (
+        <FollowFlex>
+            <div>
+                <Link to={`/profile/${username}/following`}>
+                    <p>
+            <span style={{color: theme.color}}>
               {response.following.length}
             </span>{" "}
-            <span>Following</span>
-          </p>
-        </Link>
-      </div>
-      <div>
-        <Link to={`/profile/${username}/followers`}>
-          <p>
-            <span style={{ color: theme.color }}>
+                        <span>Following</span>
+                    </p>
+                </Link>
+            </div>
+            <div>
+                <Link to={`/profile/${username}/followers`}>
+                    <p>
+            <span style={{color: theme.color}}>
               {response.followers.length}
             </span>{" "}
-            <span>Followers</span>
-          </p>
-        </Link>
-      </div>
-    </FollowFlex>
-  );
+                        <span>Followers</span>
+                    </p>
+                </Link>
+            </div>
+        </FollowFlex>
+    );
 };
 
 export default Follower;
